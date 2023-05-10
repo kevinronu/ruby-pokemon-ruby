fef9c99f580b48a8dd5e74d55b62ee287fc35b42
Kevin Robles <kevinronu@gmail.com>
2023-05-09 21:43:08 -0500

    add print_options method

--- modules/get_input.rb
+++ modules/get_input.rb
@@ -1,14 +1,21 @@
 module GetInput
-  def get_input(prompt:, msg: "", required: true)
-    print "#{prompt}: ".colorize(:light_cyan)
+  def get_input(prompt:, msg: "", required: true, color: :default)
+    print "#{prompt}: ".colorize(color)
     input = gets.chomp
     return input unless required
 
     while input.empty?
       puts msg
-      print "#{prompt}: ".colorize(:light_cyan)
+      print "#{prompt}: ".colorize(color)
       input = gets.chomp
     end
     input
+  end
+
+  def print_options(options)
+    options.each.with_index do |option, index|
+      print "#{index + 1}. #{option.capitalize}      "
+    end
+    puts ""
   end
 end
